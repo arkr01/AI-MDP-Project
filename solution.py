9d157b92ce3e39de41fbf21c9599fc5faed9c386
Adrian Rajkamal <88476164+arkr01@users.noreply.github.com>
2022-07-01 15:28:03 +1000

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,27 +6,10 @@ from game_state import GameState
 
 """
 solution.py
-
-Template file for you to implement your solution to Assignment 2.
-
-You must implement the following method stubs, which will be invoked by the simulator during 
-testing: __init__(game_env) plan_offline() select_action() 
-    
-To ensure compatibility with the autograder, please avoid using try-except blocks for Exception 
-or OSError exception types. Try-except blocks with concrete exception types other than OSError
-(e.g. try: ... except ValueError) are allowed. 
-
-COMP3702 2021 Assignment 2 Support Code
-
-Last updated by njc 02/09/21
-
-REFERENCES:
-- Tutorial 7 Solution Code (For Value/Policy/Linear Algebra Policy Iteration Implementations)
 """
 
 
 def dict_argmax(d):
-    """ FROM TUTORIAL 7 SOLUTION. GET ARGMAX FROM DICTIONARY."""
     max_value = max(d.values())
     for k, v in d.items():
         if v == max_value:
@@ -37,9 +20,7 @@ class Solver:
 
     def __init__(self, game_env):
         """
-        Constructor for your solver class.
-
-        Any additional instance variables you require can be initialised here.
+        Constructor for the solver class.
 
         Computationally expensive operations should not be included in the constructor,
         and should be placed in the plan_offline() method instead.
@@ -61,9 +42,6 @@ class Solver:
     def plan_offline(self):
         """
         This method will be called once at the beginning of each episode.
-
-        You can use this method to perform value iteration and/or policy iteration and store the
-        computed policy, or (optionally) to perform pre-processing for MCTS.
 
         This planning should not depend on the initial state, as during simulation this is not
         guaranteed to match the initial position listed in the input file (i.e. you may be given
@@ -121,10 +99,6 @@ class Solver:
         """
         This method will be called each time the agent is called upon to decide which action to
         perform (once for each step of the episode).
-
-        You can use this to retrieve the optimal action for the current state from a stored
-        offline policy (e.g. from value iteration or policy iteration), or to perform MCTS
-        simulations from the current state.
 
         The allowed run time for this method is given by 'game_env.online_time'. The method will
         be terminated by the simulator if it does not complete within this limit - you should
